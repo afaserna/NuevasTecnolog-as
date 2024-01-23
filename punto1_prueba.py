a56989046aac6b2e1d4c6a2eee80386c209ff302
andres arboleda <afaserna@gmail.com>
2024-01-23 17:33:29 -0500

    correcion 3 para cuando no hay extras

--- punto1_prueba.py
+++ punto1_prueba.py
@@ -5,7 +5,7 @@ from colorama import Fore, Style
 Nombre = str (input ("ingrese el nombre del empleado: "))
 Nro_hijos= int (input ("ingrese el numero de hijos: "))
 Valor_hora = 10000
-Horas_trabajadas = float (input("ingrese numero de horas trabajadas por semana: "))
+Horas_trabajadas = int (input("ingrese numero de horas trabajadas por semana: "))
 Valor_extra = (Valor_hora * 0.35)+Valor_hora
 
 
@@ -20,6 +20,7 @@ if Horas_trabajadas > 47:
     
 else: 
     cantidad_extras = 0
+    Valor_extra = 0
     
 if Horas_trabajadas >= 1:  
     valor_Hsemana= 47 *Valor_hora   
