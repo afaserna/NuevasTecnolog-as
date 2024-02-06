45c951faedf5ac761a0c8fa7ade2cf54c9285aef
andres arboleda <afaserna@gmail.com>
2024-02-06 10:24:18 -0500

    c2 Extras

--- pruebaSalario.py
+++ pruebaSalario.py
@@ -1,6 +1,11 @@
 
 Nombre = str (input ("ingrese el nombre del empleado: "))
-Saalrio= int (input ("ingrese el salario: "))
+Salario= int (input ("ingrese el salario: "))
+
+numeroH_Extras = int (input("ingrese las horas extras"))
+valor_hora= (Salario / 30)
+Valor_extra = (valor_hora * 0.35)+ valor_hora
+total_Extras = Valor_extra * numeroH_Extras
 
 
 
